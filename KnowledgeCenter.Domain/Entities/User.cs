1497866eeb2d0fc54a0d034f5786c72afb50c266
1enzz <ooliveiraenzo5@gmail.com>
2026-09-07 19:07:43 -0300

    Adicionar arquivos de projeto.

---
+++ KnowledgeCenter.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace KnowledgeCenter.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int PhoneNumber { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+        public string Document { get; set; }
+    }
+}
